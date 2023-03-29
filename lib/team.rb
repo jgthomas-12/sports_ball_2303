54dbf257f30c8b3b5d4455446c5a1f4a8953cdb5
Jesse Thomas <jgthomas3@gmail.com>
2023-03-29 10:56:38 -0600

    Add first steps and ideas for long_term_players method team.rb

--- lib/team.rb
+++ lib/team.rb
@@ -9,4 +9,10 @@ class Team
     @roster << player 
     @player_count = roster.count
   end
+
+  def long_term_players
+    # iterate through the roster 
+    # divide contract_rate/12
+    # return players with 3 or more years
+  end
 end
