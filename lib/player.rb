2ef7b9890548d689411f5097a05decd8ffa410fc
Jesse Thomas <jgthomas3@gmail.com>
2023-03-29 09:56:55 -0600

    Add first_name method

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,17 @@
 class Player
+  attr_reader :name
   def initialize(name, monthly_cost, contract_length)
-    
+    @name = name
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
   end
+
+  def first_name
+    both = []
+    both << name.split(" ")
+    both.flatten!
+    both[0]
+    # require 'pry'; binding.pry
+  end
+  
 end
